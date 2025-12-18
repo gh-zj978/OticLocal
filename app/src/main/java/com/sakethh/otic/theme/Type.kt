6eb7f49955fd388fb1aecff701910becc91b61ed
sakethpathike <sakethpathike@gmail.com>
2025-12-18 20:32:30 +0530

    feat: use custom subsetting(s) from the Google Sans Flex font

--- app/src/main/java/com/sakethh/otic/theme/Type.kt
+++ app/src/main/java/com/sakethh/otic/theme/Type.kt
@@ -1,54 +1,18 @@
 package com.sakethh.otic.theme
 
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import com.sakethh.otic.R
 
-@OptIn(ExperimentalTextApi::class)
 val googleSansFlexFontFamily = FontFamily(
     Font(
-        resId = R.font.google_sans_flex,
-        weight = FontWeight.Normal,
-        variationSettings = FontVariation.Settings(
-            FontVariation.weight(400)
-        )
-    ),
-    Font(
-        resId = R.font.google_sans_flex,
-        weight = FontWeight.Medium,
-        variationSettings = FontVariation.Settings(
-            FontVariation.weight(500)
-        )
-    ),
-    Font(
-        resId = R.font.google_sans_flex,
-        weight = FontWeight.SemiBold,
-        variationSettings = FontVariation.Settings(
-            FontVariation.weight(600)
-        )
-    ),
-    Font(
-        resId = R.font.google_sans_flex,
-        weight = FontWeight.Bold,
-        variationSettings = FontVariation.Settings(
-            FontVariation.weight(700)
-        )
-    ),
-    Font(
-        resId = R.font.google_sans_flex,
-        weight = FontWeight.ExtraBold,
-        variationSettings = FontVariation.Settings(
-            FontVariation.weight(800)
-        )
-    ),
-    Font(
-        resId = R.font.google_sans_flex,
-        weight = FontWeight.Black,
-        variationSettings = FontVariation.Settings(
-            FontVariation.weight(900)
-        )
+        resId = R.font.gsf_120pt_regular, weight = FontWeight.Normal
+    ), Font(
+        resId = R.font.gsf_120pt_medium, weight = FontWeight.Medium
+    ), Font(
+        resId = R.font.gsf_120pt_semibold, weight = FontWeight.SemiBold
+    ), Font(
+        resId = R.font.gsf_120pt_bold, weight = FontWeight.Bold
     )
 )
